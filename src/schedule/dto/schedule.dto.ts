5e6a285d6864030511bec6b355f96fd016fc7635
Xaletch <kiril.kolesnikov0609@mail.ru>
2026-07-10 14:17:17 +0300

    schedule bug fixed

--- src/schedule/dto/schedule.dto.ts
+++ src/schedule/dto/schedule.dto.ts
@@ -1,6 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsArray, IsString, Matches, ValidateNested } from "class-validator";
+import {
+  IsArray,
+  IsDateString,
+  IsString,
+  Matches,
+  ValidateNested,
+} from "class-validator";
 
 export type Interval = {
   start: string;
@@ -17,7 +23,7 @@ export class IntervalDto {
   @Matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/, {
     message: "Время должно быть в формате HH:MM",
   })
-  start: string;
+  start!: string;
 
   @ApiProperty({
     example: "22:15",
@@ -28,20 +34,21 @@ export class IntervalDto {
   @Matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/, {
     message: "Время должно быть в формате HH:MM",
   })
-  end: string;
+  end!: string;
 }
 
 export class ScheduleDto {
   @ApiProperty({
-    example: "31-12-2025",
-    description: "Дата в формате DD-MM-YYYY",
+    example: "24-11-2025",
+    description: "Дата в формате YYYY-MM-DD",
     required: true,
   })
-  @IsString()
-  @Matches(/^\d{2}-\d{2}-\d{4}$/, {
-    message: "Дата должна быть в формате DD-MM-YYYY",
-  })
-  date: string;
+  // @Matches(/^\d{2}-\d{2}-\d{4}$/, {
+  //   message: "Дата должна быть в формате DD-MM-YYYY",
+  // })
+  // date!: string;
+  @IsDateString()
+  date!: string;
 
   @ApiProperty({
     type: [IntervalDto],
@@ -52,7 +59,7 @@ export class ScheduleDto {
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => IntervalDto)
-  intervals: IntervalDto[];
+  intervals!: IntervalDto[];
 
   @ApiProperty({
     example: "89e1ff87-f273-47a4-ab34-a90c716c59f0",
@@ -60,5 +67,5 @@ export class ScheduleDto {
     required: true,
   })
   @IsString()
-  user_id: string;
+  user_id!: string;
 }
